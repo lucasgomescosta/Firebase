88961c080533b7b8c4d0e0dbecf1399c3cd372e1
lucasgomescosta <lucasgomescosta@users.noreply.github.com>
2019-11-29 00:32:20 -0400

    update cadastro firebase

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
 import firebase from 'firebase';
 
 
@@ -8,8 +8,13 @@ export default class App extends Component {
 
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {
+      //cliente: 'Carregando..'
+      nome: '',
+      cargo: ''
+    };
 
+    this.cadastrar = this.cadastrar.bind(this);
     let firebaseConfig = {
       apiKey: "AIzaSyDHwMpUfXp8tBtxcnyQh36QuL78GB5CvgQ",
       authDomain: "myapp-b66a3.firebaseapp.com",
@@ -22,13 +27,73 @@ export default class App extends Component {
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
+    //add um ouvinte
+   /* firebase.database().ref("cliente").on("value", (snapshort) => {
+        let state = this.state;
+        state.cliente = snapshort.val();
+        this.setState(state);
+    });*/
+
+    //pegando valores dos campos usuarios
+   /* firebase.database().ref("usuarios/id/idade").on("value", (snapshort) => {
+      let state = this.state;
+      state.cliente = snapshort.val();
+      this.setState(state);
+    });*/
+
+    //carregar uma única vez
+  /*  firebase.database().ref("cliente").once("value", (snapshort) => {
+      let state = this.state;
+      state.cliente = snapshort.val();
+      this.setState(state);
+  });*/
 
   };
 
+  cadastrar() {
+    if(this.state.nome != '' && this.state.cargo != ''){
+
+      let usuarios = firebase.database().ref('usuarios');
+      let chave =usuarios.push().key;
+
+      usuarios.child(chave).set({
+        nome: this.state.nome,
+        cargo: this.state.cargo
+      });
+
+      alert("Cadastrado com sucesso!!");
+
+    }
+  }
+
+
+  //{this.state.cliente}
   render() {
     return (
       <View style={styles.container}>
-        <Text>Welcome to React Native!</Text>
+        <View style={styles.inputView}>
+          <TextInput
+            underlineColorAndroid="transparent"
+            value={this.props.email}  
+            style={styles.inputText}
+            placeholder="Nome..." 
+            placeholderTextColor="#003f5c"
+            onChangeText={(nome) => this.setState({nome})}   
+          />
+        </View>
+        <View style={styles.inputView}>
+           <TextInput
+           underlineColorAndroid="transparent"
+            value={this.props.email}  
+            style={styles.inputText}
+            placeholder="Cargo..." 
+            placeholderTextColor="#003f5c"
+            onChangeText={(cargo) => this.setState({cargo})}   
+          />
+        </View>
+        <TouchableOpacity style={styles.loginBtn} onPress={this.cadastrar}>
+          <Text style={styles.loginText}>Cadastrar Novo Funcionário</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -37,6 +102,35 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#003f5c',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
+  inputView:{
+    width:"80%",
+    backgroundColor:"#465881",
+    borderRadius:25,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20
+  },
+  inputText:{
+    height:50,
+    color:"white"
+  },
+  loginBtn:{
+    width:"80%",
+    backgroundColor:"#fb5b5a",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:10
+  },
+  loginText:{
+    color:"white"
+  }
   
 });
